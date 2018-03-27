4be393748f1057877031213c4e1c2a3da8cbc99b
David Markel <davidgmarkel@gmail.com>
2018-03-27 16:05:23 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,7 +5,18 @@ attr_reader :name
   def initialize(name)
     @name = name
     @balance = 1000
-    @status = open
+    @status = "open"
   end
+
+  def deposit(money)
+    @balance += money
+  end
+
+  def display_balance
+    @balance
+  end
+
+  def valid?
+    @balance > 0 && @status == "open" 
 
 end
