3c0300d8f78501c99161bcf2d84b8ac70ddac095
David Markel <davidgmarkel@gmail.com>
2018-03-27 16:02:02 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,8 @@
 class BankAccount
-attr_reader :account_holder
+attr_reader :name
 
-  def initialize(account_holder)
-    @account_holder = account_holder
+  def initialize(name)
+    @account_holder = name
   end
 
 end
